45c4f0a5b80daec484a8994b177c030fff10d584
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-21 08:24:04 -0300

    🚧 update imports app module

--- todo-list-backend/src/app.module.ts
+++ todo-list-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { TaskModule } from './modules/task/task.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule,TaskModule, CategoryModule],
   controllers: [],
   providers: [PrismaService],
 })
