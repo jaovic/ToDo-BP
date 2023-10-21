ea785380327c1551eedd1d8a8f638c63deea030f
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-21 02:58:28 -0300

    🚧 add auth module

---
+++ todo-list-backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthRepository } from "./repository/auth.repository";
+import { AuthService } from "./service/auth.service";
+import { PrismaService } from "src/prisma.service";
+import { JwtModule } from "@nestjs/jwt";
+import { SmsService } from "../sms/sms.service";
+import { localStrategy } from "./estrategies/local.strategy";
+import { JwtStrategy } from "./estrategies/jwt.strategy";
+import { ConfigModule } from "@nestjs/config";
+import { PassportModule } from "@nestjs/passport";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    PassportModule,
+    JwtModule.register({
+      privateKey: process.env.TOKEN_SECRET,
+      signOptions: { expiresIn: '1m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    localStrategy,
+    JwtStrategy,
+    SmsService,
+    PrismaService,
+    AuthRepository
+  ],
+})
+export class AuthModule {}
