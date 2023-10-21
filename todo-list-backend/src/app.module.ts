3ea966d727f81edd8df7175eb33d2894c1c0c6e6
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-21 02:56:05 -0300

    🚧 update imports and providers module

--- todo-list-backend/src/app.module.ts
+++ todo-list-backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
