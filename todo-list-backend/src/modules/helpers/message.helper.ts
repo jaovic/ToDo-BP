61d84177ca67ba49e2a66a63af1366c5b829f698
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-21 03:02:01 -0300

    🚧 add helpers

---
+++ todo-list-backend/src/modules/helpers/message.helper.ts
@@ -0,0 +1,14 @@
+export const MessagesHelper = {
+    NAME_REQUIRED: 'Name is required',
+    NAME_INVALID: 'Name is invalid',
+    EMAIL_REQUIRED: 'Email is required',
+    EMAIL_INVALID: 'Email is invalid',
+    PASSWORD_REQUIRED: 'Password is required',
+    PASSWORD_OR_EMAIL_INVALID: 'Password or email is invalid',
+    PASSWORD_VALID:'The password must contain uppercase and lowercase letters, numbers and special characters',
+    CPF_REQUIRED: 'CPF is required',
+    CPF_INVALID: 'CPF is invalid',
+    PHONE_REQUIRED: 'Phone is required',
+    PHONE_INVALID: 'Phone is invalid',
+    CODE_REQUIRED: 'Code is required',
+}
