c0613501de9c70b5417708ba8038929283a1efa6
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-23 06:35:18 -0300

    ♻️ add try catch and pagination logic repository

--- todo-list-backend/src/modules/task/repository/task.repository.ts
+++ todo-list-backend/src/modules/task/repository/task.repository.ts
@@ -1,63 +1,109 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma.service";
-import { ICreateTaskRepository, ITaskRepository, IUpdateTaskRepository } from "../structure/IRepository.structure";
-import { Task } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import {
+  ICreateTaskRepository,
+  IFindTasksPaginationRepository,
+  ITaskRepository,
+  IUpdateTaskRepository,
+} from '../structure/IRepository.structure';
+import { Task } from '@prisma/client';
 
 @Injectable()
 export class TaskRepository implements ITaskRepository {
   constructor(private prisma: PrismaService) {}
 
   async create(data: ICreateTaskRepository, categoryId: string): Promise<Task> {
-    return await this.prisma.task.create({
-      data:{
-        name: data.name,
-        description: data.description,
-        userId: data.userId,
-        categoryId
-      }
-    })
+    try {
+      return await this.prisma.task.create({
+        data: {
+          name: data.name,
+          description: data.description,
+          userId: data.userId,
+          categoryId,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
   }
 
-  async findTasks(id: string): Promise<Task[]> {
-    return this.prisma.task.findMany({where:{userId: id},include:{category:{select:{name: true}}}})
+  async findTasks(
+    id: string,
+    pagination: IFindTasksPaginationRepository,
+  ): Promise<Task[]> {
+    const LIMIT_PER_PAGE = 10;
+    const offset = Number(pagination.page - 1) * LIMIT_PER_PAGE;
+    try {
+      return this.prisma.task.findMany({
+        skip: offset,
+        take: LIMIT_PER_PAGE,
+        where: {
+          userId: id,
+        },
+        include: {
+          category: {
+            select: {
+              name: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
   }
 
-  async delete(id: string): Promise<true>{
-  await this.prisma.task.delete({ where: { id } });
-    return true;
+  async delete(id: string): Promise<true> {
+    try {
+      await this.prisma.task.delete({ where: { id } });
+      return true;
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
   }
 
   async update(data: IUpdateTaskRepository): Promise<Task> {
-    return this.prisma.task.update({
-      where:{
-        id: data.id
-      },
-      data:{
-        name: data.name,
-        description: data.description
-      }
-    })
+    try {
+      return this.prisma.task.update({
+        where: {
+          id: data.id,
+        },
+        data: {
+          name: data.name,
+          description: data.description,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
   }
 
   async changeStatus(id: string): Promise<Task> {
-    const existTask = await this.prisma.task.findUnique({
-      where: {
-        id,
-      },
-    });
+    try {
+      const existTask = await this.prisma.task.findUnique({
+        where: {
+          id,
+        },
+      });
 
-    if (!existTask) {
-      throw new Error(`Tarefa com o ID ${id} não encontrada.`);
+      if (!existTask) {
+        throw new Error(`Tarefa com o ID ${id} não encontrada.`);
+      }
+
+      const newStatus = !existTask.status;
+      return await this.prisma.task.update({
+        where: {
+          id,
+        },
+        data: {
+          status: newStatus,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
     }
-
-    const newStatus = !existTask.status;
-    return await this.prisma.task.update({
-      where: {
-        id,
-      },
-      data: {
-        status: newStatus,
-      },
-    });
   }
 }
