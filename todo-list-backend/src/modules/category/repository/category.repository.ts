ac054bd552e76a5808bbaa97ed8a25e1459f584b
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-23 06:36:53 -0300

    🚧 add try catch

--- todo-list-backend/src/modules/category/repository/category.repository.ts
+++ todo-list-backend/src/modules/category/repository/category.repository.ts
@@ -1,18 +1,32 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma.service";
-import { ICategoryRepository, ICreateCategory, IFindCategory } from "../structure/IRepository.structure";
-import { Category } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import {
+  ICategoryRepository,
+  ICreateCategory,
+  IFindCategory,
+} from '../structure/IRepository.structure';
+import { Category } from '@prisma/client';
 
 @Injectable()
 export class CategoryRepository implements ICategoryRepository {
   constructor(private prisma: PrismaService) {}
 
   async create(data: ICreateCategory): Promise<Category> {
-    return this.prisma.category.create({data})
-}
-async findCategory(data: IFindCategory): Promise<Category>{
-    return this.prisma.category.findFirst({where:{
-        name: data.name
-    }})
-}
+    try {
+      return this.prisma.category.create({ data });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
+  }
+  async findCategory(data: IFindCategory): Promise<Category> {
+    try {
+      return this.prisma.category.findFirst({
+        where: {
+          name: data.name,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Prisma Error: ${error}`);
+    }
+  }
 }
