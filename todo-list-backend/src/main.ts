1429313eb549577073bec8805e66c6f6162dff5f
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-25 23:45:18 -0300

    🚧 add pipe to acept local requisitions cors error

--- todo-list-backend/src/main.ts
+++ todo-list-backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.use(
+    cors({
+      origin: 'http://127.0.0.1:5173',
+      credentials: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
