1afb6bc0b0dd61d8cdf4e34b5123f17cf9346c9e
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-26 06:24:27 -0300

    🚧 update logic create task return

--- todo-list-backend/src/modules/task/service/writing/crateTask.service.ts
+++ todo-list-backend/src/modules/task/service/writing/crateTask.service.ts
@@ -1,41 +1,48 @@
-import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
-import { ICreateTask, ICreateTaskService } from "../../structure/IService.structure";
-import { TaskRepository } from "../../repository/task.repository";
-import { ITaskRepository } from "../../structure/IRepository.structure";
-import { CategoryRepository } from "src/modules/category/repository/category.repository";
-import { ICategoryRepository } from "src/modules/category/structure/IRepository.structure";
-import { Task } from "@prisma/client";
-import { AuthRepository } from "src/modules/auth/repository/auth.repository";
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import {
+  ICreateTask,
+  ICreateTaskService,
+} from '../../structure/IService.structure';
+import { TaskRepository } from '../../repository/task.repository';
+import { ITaskRepository } from '../../structure/IRepository.structure';
+import { CategoryRepository } from 'src/modules/category/repository/category.repository';
+import { ICategoryRepository } from 'src/modules/category/structure/IRepository.structure';
+import { Task } from '@prisma/client';
+import { AuthRepository } from 'src/modules/auth/repository/auth.repository';
 
 @Injectable()
 export class CreateTaskService implements ICreateTaskService {
-
   constructor(
     @Inject(TaskRepository)
     private readonly taskRepository: ITaskRepository,
     @Inject(CategoryRepository)
     private readonly categoryRepository: ICategoryRepository,
     @Inject(AuthRepository)
-    private readonly authRepository: AuthRepository
+    private readonly authRepository: AuthRepository,
   ) {}
   async execute(data: ICreateTask): Promise<Task> {
-    const user = await this.authRepository.findById(data.userId)
+    const user = await this.authRepository.findById(data.userId);
 
-    if(!user){
+    if (!user) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    const categoryExist = await this.categoryRepository.findCategory({name: data.category})
+    const categoryExist = await this.categoryRepository.findCategory({
+      name: data.category,
+    });
 
-    let categoryId: string
-    
-    if(!categoryExist){
-      const category = await this.categoryRepository.create({name: data.category})
-      categoryId = category.id
+    let categoryId: string;
+
+    if (!categoryExist) {
+      const category = await this.categoryRepository.create({
+        name: data.category,
+      });
+      categoryId = category.id;
+      return this.taskRepository.create(data, categoryId);
     }
 
-    categoryId = categoryExist.id
+    categoryId = categoryExist.id;
 
-    return this.taskRepository.create(data,categoryId)
+    return this.taskRepository.create(data, categoryId);
   }
 }
