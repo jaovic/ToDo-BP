8a9ad305c40561928463444e3df45e03eaf4eb3b
João Victor Martins <j_victorfernandes@hotmail.com>
2023-10-26 10:16:15 -0300

    📝 add route swagger documentation

--- todo-list-backend/src/main.ts
+++ todo-list-backend/src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocs from './common/swagger/swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
